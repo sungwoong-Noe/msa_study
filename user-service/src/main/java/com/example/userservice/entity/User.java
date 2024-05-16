86e3bf241043cf829c1dd9d6cfdf8e350567a300
nohsw <asdz453@gmail.com>
2024-05-16 22:21:22 +0900

    user controller, env 설정파일

---
+++ user-service/src/main/java/com/example/userservice/entity/User.java
@@ -0,0 +1,18 @@
+package com.example.userservice.entity;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+}
