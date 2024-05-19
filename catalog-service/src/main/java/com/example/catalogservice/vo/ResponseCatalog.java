69e767085fd714ed8fad9c92545d6ae78c851f94
nohsw <asdz453@gmail.com>
2024-05-19 21:09:26 +0900

    FEAT: catalog vo, dto 생성

---
+++ catalog-service/src/main/java/com/example/catalogservice/vo/ResponseCatalog.java
@@ -0,0 +1,19 @@
+package com.example.catalogservice.vo;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ResponseCatalog {
+
+
+    private String productId;
+    private Integer productName;
+    private Integer unitPrice;
+    private Integer stock;
+    private LocalDate createAt;
+
+}
